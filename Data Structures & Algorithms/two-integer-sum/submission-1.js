5b9a7d128e83db104474530f6a72ec467f66618d
Lam Nguyen <nguyenchanhlam0811@gmail.com>
2026-04-06 17:44:25 +0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map();
+
+        for(let i =0; i<nums.length; i++) {
+          const difference = target - nums[i];
+
+            if(map.has(difference)) {
+                return [i, map.get(difference)]
+            } else {
+                map.set(nums[i], i)
+            }
+        }
+        return []
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+}
+ 
